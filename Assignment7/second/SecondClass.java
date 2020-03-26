6d1266f2a2b6b9d045fbcd8b055e3914c3e6fbb8
hemasri098 <59275471+hemasri098@users.noreply.github.com>
2020-03-26 10:13:28 +0530

    added Exception handling

--- Assignment7/second/SecondClass.java
+++ Assignment7/second/SecondClass.java
@@ -1,13 +1,14 @@
 package com.company.JavaAssignment7.second;
 import java.util.logging.Logger;
 
+//cycle class
 class Cycle {
     Logger LOGGER = Logger.getAnonymousLogger();
     void balance() {
         LOGGER.info("balancing cycle");
     }
 }
-
+//subclasses extending cycle class
 class UniCycle extends Cycle {
     Logger LOGGER = Logger.getAnonymousLogger();
     public UniCycle() {
@@ -35,22 +36,24 @@ class TriCycle extends Cycle {
 }
 
 public class SecondClass {
+    //method to show upcasting
     public static void upcast(Cycle[] arrayOfCycleObjects) {
         UniCycle unicycle = new UniCycle();
         BiCycle bicycle = new BiCycle();
         TriCycle tricycle = new TriCycle();
-        arrayOfCycleObjects[0] = (Cycle)unicycle;
+        arrayOfCycleObjects[0] = (Cycle)unicycle;//upcasting
         arrayOfCycleObjects[1] = (Cycle)bicycle;
         arrayOfCycleObjects[2] = (Cycle)tricycle;
         arrayOfCycleObjects[0].balance();
         arrayOfCycleObjects[1].balance();
         arrayOfCycleObjects[2].balance();
     }
+    //method for downcasting
     public static void downcast(Cycle[] arrayOfCycleObjects) {
         Cycle unicycle = new UniCycle();
         Cycle bicycle = new BiCycle();
         Cycle tricycle = new TriCycle();
-        arrayOfCycleObjects[0] = (UniCycle)unicycle;
+        arrayOfCycleObjects[0] = (UniCycle)unicycle;//downcasting
         arrayOfCycleObjects[1] = (BiCycle)bicycle;
         arrayOfCycleObjects[2] = (TriCycle)tricycle;
         arrayOfCycleObjects[0].balance();
@@ -59,7 +62,13 @@ public class SecondClass {
     }
     public static void main(String[]args) {
         Cycle[] arrayOfCycleObjects = new Cycle[3]; //this does not create the objects
-        upcast(arrayOfCycleObjects);
-        downcast(arrayOfCycleObjects);
+        Logger LOGGER = Logger.getAnonymousLogger();
+        try {
+            upcast(arrayOfCycleObjects);
+            downcast(arrayOfCycleObjects);
+        }
+        catch(Exception e) {
+            LOGGER.info("Exception: "+ e);
+        }
     }
 }
