292443c0a6f962a78cdeec486448896b91ceb1ff
hemasri098 <59275471+hemasri098@users.noreply.github.com>
2020-03-26 10:29:25 +0530

    added loggers and exception handling

--- Assignment7/fifth/FifthClass.java
+++ Assignment7/fifth/FifthClass.java
@@ -1,15 +1,16 @@
-package com.company.Assignment7.fifth;
-
+package com.company.JavaAssignment7.fifth;
+import java.util.logging.Logger;
 
 class OuterClass_1 {
     class InnerClass_1 {
+        Logger LOGGER = Logger.getAnonymousLogger();
         InnerClass_1(String str) {
-            System.out.println("Innerclass constructor with parameter " + str);
+            LOGGER.info("Innerclass constructor with parameter " + str);
         }
     }
 }
 class OuterClass_2 extends OuterClass_1{
-    class InnerClass_2  extends OuterClass_1.InnerClass_1{
+    class InnerClass_2  extends InnerClass_1{
         InnerClass_2(String str) {
             super(str);
         }
@@ -18,8 +19,14 @@ class OuterClass_2 extends OuterClass_1{
 
 public class FifthClass {
     public static void main(String[] args) {
-        OuterClass_2 outclass1 = new OuterClass_2();
-        OuterClass_2.InnerClass_2 in = outclass1.new InnerClass_2("string");
+        Logger LOGGER = Logger.getAnonymousLogger();
+        try {
+            OuterClass_2 outclass1 = new OuterClass_2();
+            OuterClass_2.InnerClass_2 in = outclass1.new InnerClass_2("string");
+        }
+        catch (Exception e) {
+            LOGGER.info("Exception: " + e);
+        }
     }
 }
 
